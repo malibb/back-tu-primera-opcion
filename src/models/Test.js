338ca308fea49fb63769eb916d49c85cebc67765
Malinali <mali@devf.mx>
2019-10-26 12:54:46 -0500

    models definition

---
+++ src/models/Test.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const TestSchema = new Schema({
+
+    watson_audio: {
+        type: String,
+    },
+    watson_personalities: {
+        type: Object,
+        require:true
+    },
+    watson_tech: {
+        type: Object
+    },
+    is_active: {
+        type: Boolean,
+        default: true
+    }
+
+}, {
+    timestamps: true
+});
+
+module.exports = mongoose.model('tests', TestSchema);
---
+++ src/models/User.js
@@ -0,0 +1,65 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+
+    first_name: {
+        type: String,
+        required: true
+    },
+    last_name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+
+    },
+    password: {
+        type: String
+    },
+    birth_date: {
+        type: Date //2019-09-25T20:00:00
+    },
+    gender: {
+        type: String,
+        enum: ['M', 'F', 'O']
+    },
+    tests: {
+        type: [Schema.Types.ObjectId],
+        ref: 'tests'
+    },
+    profile_pics: {
+        type: [String]
+    },
+    is_active: {
+        type: Boolean,
+        default: true
+    }
+
+}, {
+    timestamps: true
+});
+
+UserSchema.pre('save', function (next) {
+    const user = this;
+    const SALT_FACTOR = 10;
+    if (!user.isModified('password')) {
+        return next();
+    }
+    bcrypt.genSalt(SALT_FACTOR, function (err, salt) {
+        if (err) return next(err);
+        bcrypt.hash(user.password, salt, function (err, hash) {
+            if (err) return next(err);
+            user.password = hash;
+            next();
+        });
+    });
+
+});
+
+module.exports = mongoose.model('Users', UserSchema);
